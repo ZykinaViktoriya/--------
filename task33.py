5f711fe654b447ed42b2ca0fcf9ad4b848baf78b
ZykinaViktoriya <zykina.viktoriya@gmail.com>
2023-04-20 20:51:52 +0300

    new file

--- task33.py
+++ task33.py
@@ -2,3 +2,18 @@
 # Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
 # Input: 5 -> 1 3 3 3 4
 # Output: 1 3 3 3 1
+
+import random
+n = int(input('Введите количество оценок: '))
+lst = [random.randint(1, 5) for i in range(n)]
+print(lst)
+max_mark = max(lst)
+min_mark = min(lst)
+
+print(list(enumerate(lst)))
+
+for i, val in enumerate(lst):
+    if val == max_mark:
+        lst[i] = min_mark
+
+print(*lst)
